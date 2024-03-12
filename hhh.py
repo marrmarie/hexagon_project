33e07e93bc7a7c0c841d1bef7277c5c083df7ba8
marrmarie <mashusy2008@gmail.com>
2024-03-12 16:43:04 +0300

    поле рисуется правильно

--- hhh.py
+++ hhh.py
@@ -3,13 +3,12 @@ from collections import deque
 from random import choice
 
 WIDTH = 1200
-HEIGHT = 750
+HEIGHT = 850
 SIZE = [WIDTH, HEIGHT]
-a = 10
+a = 35
 
 HEX_CONST = (3 / 4) ** 0.5
-# half = HEX_CONST * a
-half = 20
+half = HEX_CONST * a
 hei = 0.5 * a
 
 flag = 1
@@ -24,21 +23,27 @@ class Hexagon:
         self.visited = False
 
     def paint(self):
-        pg.draw.line(screen, 'black', (self.x, self.y), (self.x + half, self.y - hei))
+        if self.walls[0]:
+            pg.draw.line(screen, 'black', (self.x, self.y), (self.x + half, self.y - hei))
         x1 = self.x + half
         y1 = self.y - hei
-        pg.draw.line(screen, 'black', (x1, y1), (x1 + half, y1 + hei))
+        if self.walls[1]:
+            pg.draw.line(screen, 'black', (x1, y1), (x1 + half, y1 + hei))
         x1 = x1 + half
         y1 = y1 + hei
-        pg.draw.line(screen, 'black', (x1, y1), (x1, y1 + a))
+        if self.walls[2]:
+            pg.draw.line(screen, 'black', (x1, y1), (x1, y1 + a))
         y1 += a
-        pg.draw.line(screen, 'black', (x1, y1), (x1 - half, y1 + hei))
+        if self.walls[3]:
+            pg.draw.line(screen, 'black', (x1, y1), (x1 - half, y1 + hei))
         x1 = x1 - half
         y1 = y1 + hei
-        pg.draw.line(screen, 'black', (x1, y1), (x1 - half, y1 - hei))
+        if self.walls[4]:
+            pg.draw.line(screen, 'black', (x1, y1), (x1 - half, y1 - hei))
         x1 = x1 - half
         y1 = y1 - hei
-        pg.draw.line(screen, 'black', (x1, y1), (x1, y1 - a))
+        if self.walls[5]:
+            pg.draw.line(screen, 'black', (x1, y1), (x1, y1 - a))
 
 pg.init()
 
@@ -58,26 +63,25 @@ while True:
     x_paint = 600 - half
     rows = (1500 - a) // (3 * a)
     ost = (1500 - a) % (3 * a)
-    y_paint = ost / 2
+    y_paint = max(ost / 2, 0.5 * a)
     rise = True
-    first_row = 1
-    for i in range(rows):
-        print(first_row)
+    count_row = 1
+    for i in range(rows + 3):
         x_f = x_paint
         y_f = y_paint
-        for j in range(first_row):
+        for j in range(count_row):
             h = Hexagon(x_paint, y_paint)
             h.paint()
             x_paint += 2 * half
 
-        if first_row > rows // 2 + 1:
+        if count_row > rows // 2 + 1:
             rise = False
         if rise:
-            first_row += 1
+            count_row += 1
             x_paint = x_f - half
             y_paint = y_f + 1.5 * a
         else:
-            first_row -= 1
+            count_row -= 1
             x_paint = x_f + half
             y_paint = y_f + 1.5 * a
 
