aa7f35a490b25004049df2f1980c4facd4ec5824
marrmarie <mashusy2008@gmail.com>
2024-03-12 16:00:54 +0300

    рисуется неполное поле

--- hhh.py
+++ hhh.py
@@ -1,17 +1,18 @@
 import pygame as pg
-# from collections import deque
-# from random import choice
+from collections import deque
+from random import choice
 
 WIDTH = 1200
 HEIGHT = 750
 SIZE = [WIDTH, HEIGHT]
-a = 50
-columns = WIDTH // a
-rows = HEIGHT // a
+a = 10
+
 HEX_CONST = (3 / 4) ** 0.5
-half = HEX_CONST * a
+# half = HEX_CONST * a
+half = 20
 hei = 0.5 * a
 
+flag = 1
 
 
 
@@ -53,19 +54,32 @@ while True:
             exit()
         if i.type == pg.MOUSEBUTTONDOWN:
             print(pg.mouse.get_pos())
-    x_paint = 600
-    y_paint = 100
+
+    x_paint = 600 - half
+    rows = (1500 - a) // (3 * a)
+    ost = (1500 - a) % (3 * a)
+    y_paint = ost / 2
+    rise = True
     first_row = 1
-    for i in range(7):
+    for i in range(rows):
+        print(first_row)
         x_f = x_paint
         y_f = y_paint
         for j in range(first_row):
             h = Hexagon(x_paint, y_paint)
             h.paint()
             x_paint += 2 * half
-        x_paint = x_f - half
-        y_paint = y_f + 1.5 * a
-        first_row += 1
+
+        if first_row > rows // 2 + 1:
+            rise = False
+        if rise:
+            first_row += 1
+            x_paint = x_f - half
+            y_paint = y_f + 1.5 * a
+        else:
+            first_row -= 1
+            x_paint = x_f + half
+            y_paint = y_f + 1.5 * a
 
     pg.display.flip()
     time.tick(100)
